6a35a6a5ae3fc7a4f27bd80c2ab528d37b4eff57
RafaelDiazAnguita <55484240+RafaelDiazAnguita@users.noreply.github.com>
2022-02-08 12:05:29 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+const menu = document.getElementById('top-menu');
+const indicator = document.getElementById('menu-indicator');
+const secciones = document.querySelectorAll('.section');
+
+let indicatorTam = menu.querySelector('.row div').offsetWidth;
+indicator.style.width = indicatorTam + 'px';
+
+let indexSeccionActiva;
+
+const observer = new IntersectionObserver((entradas, observer) => {
+    entradas.forEach(entrada =>{
+        if(entrada.isIntersecting){
+            indexSeccionActiva = [...secciones].indexOf(entrada.target);
+            indicator.style.transform = `translateX(${indicatorTam * indexSeccionActiva}px)`;
+        }
+    });
+}, {
+
+});
+
+observer.observe(document.getElementById('main-slider'));
+secciones.forEach(seccion => { observer.observe(seccion); });
+
+
+const onResize = () =>{
+    indicatorTam = menu.querySelector('.row div').offsetWidth;
+    indicator.style.width = indicatorTam + 'px';
+    indicator.style.transform = `translateX(${indicatorTam * indexSeccionActiva}px)`;
+}
+
+window.addEventListener('resize',onResize);
